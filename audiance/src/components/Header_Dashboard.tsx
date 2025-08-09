04eb135e7b7fa1eaead0031d58c03acb276d9248
Priyansh Neel <neelpriyansh@gmail.com>
2025-08-10 02:38:13 +0530

    UI/UX

--- audiance/src/components/Header_Dashboard.tsx
+++ audiance/src/components/Header_Dashboard.tsx
@@ -4,23 +4,24 @@ import { useState } from 'react';
  export default function Header_Dashboard() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='z-50 w-full px-10 flex justify-between border-b-1 border-black items-center max-w-[100%] text-white h-24'>
+    <div className='z-50 w-full px-10 flex justify-between border-b-1 border-b-gray-500 items-center max-w-[100%] text-white h-24'>
         <h1 className='text-4xl font-bold text-black'>Audiance.Ai</h1>
-        <ul className='flex flex-row gap-6 justify-center items-center text-xl text-[#000000] font-urbanist '>   
-              <div className="relative">
-      <button type="button" onClick={() => setIsOpen(!isOpen)} className="w-10 h-10 rounded-full overflow-hidden bg-gray-100 dark:bg-gray-600 cursor-pointer">
+        <ul className='flex flex-row gap-10 justify-center items-center text-xl text-[#000000] font-urbanist '>  
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-7">
+  <path d="M5.85 3.5a.75.75 0 0 0-1.117-1 9.719 9.719 0 0 0-2.348 4.876.75.75 0 0 0 1.479.248A8.219 8.219 0 0 1 5.85 3.5ZM19.267 2.5a.75.75 0 1 0-1.118 1 8.22 8.22 0 0 1 1.987 4.124.75.75 0 0 0 1.48-.248A9.72 9.72 0 0 0 19.266 2.5Z" />
+  <path fillRule="evenodd" d="M12 2.25A6.75 6.75 0 0 0 5.25 9v.75a8.217 8.217 0 0 1-2.119 5.52.75.75 0 0 0 .298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 1 0 7.48 0 24.583 24.583 0 0 0 4.83-1.244.75.75 0 0 0 .298-1.205 8.217 8.217 0 0 1-2.118-5.52V9A6.75 6.75 0 0 0 12 2.25ZM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 0 0 4.496 0l.002.1a2.25 2.25 0 1 1-4.5 0Z" clipRule="evenodd" />
+</svg>
+ 
+        <div className="relative">
+      <button type="button" onClick={() => setIsOpen(!isOpen)} className="w-12 h-12 rounded-full overflow-hidden bg-gray-100 dark:bg-gray-600 cursor-pointer">
         <span className="font-medium text-gray-600 dark:text-gray-300">PN</span>
       </button>
-      {/* Dropdown Menu */}
       {isOpen && (
         <div className="absolute z-10 right-0 mt-2 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
-          {/* User Info */}
           <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-            <div>Bonnie Green</div>
-            <div className="font-medium truncate">name@flowbite.com</div>
+            <div>Priyansh Neel</div>
+            <div className="font-medium truncate">pn4942@srmist.edu.in</div>
           </div>
-
-          {/* Menu Links */}
           <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
             <li>
               <a
