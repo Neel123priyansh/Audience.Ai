732513802baff3290076dbe1372b0086b9c14947
Priyansh Neel <neelpriyansh@gmail.com>
2025-08-10 02:56:44 +0530

    UI/UX

---
+++ audiance/src/components/Side/side_pannel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Side_pannel() {
+  return (
+    <div className='z-50 w-32 px-10 flex justify-between border-b-1 border-b-gray-500 items-center max-w-[100%] text-white h-full'> 
+        <ul>
+            <button className='bg-amber-600'>Dashboard </button>
+        </ul>
+    </div>
+  )
+}
--- audiance/src/pages/dashboard.tsx
+++ audiance/src/pages/dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Header_Dashboard from '../components/Header_Dashboard'
+import Header_Dashboard from '../components/Header/Header_Dashboard'
+import side_pannel from '../components/Side/side_pannel'
 
 export default function Dashboard() {
   return (
