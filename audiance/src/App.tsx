81deb976563dea31cf6ec52f7070755d849a09fd
Priyansh Neel <neelpriyansh@gmail.com>
2025-08-10 01:46:02 +0530

    UI/UX

--- audiance/src/App.tsx
+++ audiance/src/App.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Header_Dashboard from './components/Header_Dashboard'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/DashBoard' element={<Header_Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ audiance/src/components/Header_Dashboard.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ export default function Header_Dashboard() {
+  return (
+    <div>Header_Dashboard</div>
+  )
+}
+
