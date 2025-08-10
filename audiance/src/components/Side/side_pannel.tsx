440964d909b1b78d0532b1269ee6f1ada27a0740
Priyansh Neel <neelpriyansh@gmail.com>
2025-08-10 12:22:24 +0530

    UI/UX

--- audiance/src/components/Side/side_pannel.tsx
+++ audiance/src/components/Side/side_pannel.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Side_pannel() {
   return (
-    <div className='fixed w-60 h-full px-10 flex flex-col border-b-1 border-r-1 border-gray-500 text-white'> 
-        <ul className='max-w-full flex flex-col text-black py-10 justify-around gap-10 '>
-            <button className=' border-1 rounded-[5px] bg-white'>Dashboard </button>
+    <div className='fixed w-60 h-full flex flex-col border-b-1 border-r-1 border-gray-500 text-white'> 
+        <ul className='w-60 flex flex-col px-4 text-black py-10 text- justify-around gap-10 '>
+            <button className=' border-1 rounded-[5px] text-left p-2 bg-white'>Dashboard </button>
             <button className=''>Dashboard </button>
             <button className=''>Dashboard </button>
             <button className=''>Dashboard </button>
